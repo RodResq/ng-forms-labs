a40b86e4b73658b40bbb3b0847365c2aa96517fc
RodResq <rodrigoresque@yahoo.com.br>
2022-12-06 09:35:02 -0300

    updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,6 +48,6 @@ export class AppComponent {
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
 
-
+    this.signupForm.reset();
   }
 }
